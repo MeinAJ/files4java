e789ebbb2fc7a27827311f3f888812445efe8867
aj <bos@aj.cn>
2018-02-15 20:24:12 +0800

    上传项目

---
+++ 项目资料/aj/parent/service-news/src/main/java/com/aj/service/service/NewsService.java
@@ -0,0 +1,16 @@
+package com.aj.service.service;
+
+
+import org.springframework.stereotype.Service;
+
+/**
+ * this is a service class
+ *
+ * @author Administrator
+ * @create 2018-02-15 13:41
+ */
+
+@Service(value = "newsService")
+public class NewsService {
+
+}
